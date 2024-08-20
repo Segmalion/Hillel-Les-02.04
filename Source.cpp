0fc1f9f02ab280f67960fdd752476134fbfbfd70
Segmalion <greencapsul@gmail.com>
2024-08-20 15:49:30 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Оригінальна строка:
+	// cout << Is there a bug here ? "
+	// Не вистачало - відкриваючих лапок, та крапки з комою в кінці
+	cout << "Is there a bug here ?";
+	return 0;
+}
